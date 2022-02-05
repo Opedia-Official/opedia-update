e28918aa24aba1352dc683bb53a4c877577693e5
opedia <hannan.opedia@gmail.com>
2022-02-05 16:17:38 +0600

    banner upload

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,6 +11,7 @@ import {
   FaInstagram,
   FaLinkedinIn,
 } from "react-icons/fa";
+
 import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event";
 
 export default function Navbar() {
