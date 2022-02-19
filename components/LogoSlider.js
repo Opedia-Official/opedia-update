75103e37e1e4d057da7641b2533bbe7679445c5b
opppdia <hannan.opppdia@gmail.com>
2022-02-19 03:34:09 -0800

    done slider a

--- components/LogoSlider.js
+++ components/LogoSlider.js
@@ -8,7 +8,7 @@ import Meta from "./Meta";
 
 // import reactJS from "../public/webLogo/django.png";
 
-const reactJS = "./webLogo/react.png";
+const reactJS = "./webLogo/ReactJS logo.png";
 const nextJS = "./webLogo/Nextjs.png";
 const nodeJS = "./webLogo/nodejs.png";
 const djaongo = "./webLogo/django.png";
